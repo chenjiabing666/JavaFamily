d7b58cad356b5176437a95f84114f63f50c4ed9c
cjb <18796327106@163.com>
2021-11-03 09:29:05 +0800

    gateway整合sentinel限流

--- cloud-alibaba/gateway-provider9024/src/main/java/cn/myjszl/GatewayProviderApplication.java
+++ cloud-alibaba/gateway-provider9024/src/main/java/cn/myjszl/GatewayProviderApplication.java
@@ -3,6 +3,7 @@ package cn.myjszl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @EnableDiscoveryClient ：开启服务注册和发现
