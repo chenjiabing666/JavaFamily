3c9b9ca419a91d7daed39616e2d99e619f2a9683
cjb <18796327106@163.com>
2022-06-11 15:15:27 +0800

    删除分库分表源码

--- sharding-sphere/sharding-jdbc-01/src/main/java/com/java/family/shardingjdbc001/dao/ShopMapper.java
+++
@@ -1,9 +0,0 @@
-package com.java.family.shardingjdbc001.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
-@Mapper
-@Component
-public interface ShopMapper {
-}
